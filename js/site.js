baa833c043cff2b4be0710c480a91fedabb93e71
Regb18 <reginald.ac.barnes@gmail.com>
2023-01-18 19:12:58 -0500

    +second commit, initial Rewind changes

--- js/site.js
+++ js/site.js
@@ -1,11 +1,33 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
+// Separation of Concerns
+
+// element IDs
+// userString
+
+
+//entry point AKA controller
+function getValues() {
+    let inputString = document.getElementById('userString').value;
+
+    let reversedString = reverseAString(inputString);
+
+    displayString(reversedString);
+
+}
+
+// logic function
+// reverse a string
+function reverseAString(userString) {
+    let revString = '';
     
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    // reverse the string
+    for(let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+    return revString;
+}
+
+// view function
+function displayString(revString) {
+    document.getElementById('results').textContent = revString;
+    document.getElementById('alert').classList.remove('invisible');
 }
